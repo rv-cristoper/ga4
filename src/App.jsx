5c1023da737394db4ba5e3752356edf342eead81
cristoper <cristoper.runco@banexcoin.com>
2023-08-17 15:53:11 -0500

    prueba 4

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,20 @@ function App() {
 
   const handleCount = () => {
     setCount((count) => count + 1)
-    console.log("aaa")
+    console.log("sss")
     ReactGA.event("login", { method: "Google" });
     ReactGA.event({
       action: "click",
       category: "button",
       label: "count",
+    });
+    ReactGA.event({
+      category: 'tu categoría',
+      action: 'tu acción',
+      label: 'tu etiqueta', // opcional
+      value: 99, // opcional, debe ser un número
+      nonInteraction: true, // opcional, verdadero/falso
+      transport: 'xhr', // opcional, beacon/xhr/image
     });
   }
   return (
