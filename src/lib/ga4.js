0bcc872ec143e5b56df8685a1ce86d7b89fa55e8
cristoper <cristoper.runco@banexcoin.com>
2023-10-16 10:40:44 -0500

    aaa

--- src/lib/ga4.js
+++ src/lib/ga4.js
@@ -1,17 +1,17 @@
-import ReactGA from 'react-ga4';
+// import ReactGA from 'react-ga4';
 
 export const Deposit = () => {
-    ReactGA.event({
-        category: 'Compra',
-        action: 'Depositar',
-        label: "Compra de BTC"
-    });
+    // ReactGA.event({
+    //     category: 'Compra',
+    //     action: 'Depositar',
+    //     label: "Compra de BTC"
+    // });
 }
 
 export const Withdraw = () => {
-    ReactGA.event({
-        category: 'Venta',
-        action: 'Retirar',
-        label: "Venta de BTC"
-    });
+    // ReactGA.event({
+    //     category: 'Venta',
+    //     action: 'Retirar',
+    //     label: "Venta de BTC"
+    // });
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ReactGA from 'react-ga4';
+// import ReactGA from 'react-ga4';
 import App from './App.jsx'
 import './index.css'
 
-const TRACKING_ID = 'G-Z5NJ8QL119';
-ReactGA.initialize(TRACKING_ID);
+// const TRACKING_ID = 'G-Z5NJ8QL119';
+// ReactGA.initialize(TRACKING_ID);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
